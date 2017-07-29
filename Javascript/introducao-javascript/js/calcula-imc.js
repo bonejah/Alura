17a548da363170c5b217ec356d50825844b1e8c1
Bruno Lima <dadivajah@gmail.com>
2017-07-29 09:58:22 -0300

    Add new course Javascritp for web

---
+++ Javascript/introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,58 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i=0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	var pesoEhValido = validaPeso(peso);
+	var alturaEhValido = validaAltura(altura);
+
+	if (!pesoEhValido) {
+		pesoEhValido = false;
+		tdImc.textContent = "Peso inválido";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (!alturaEhValido) {
+		alturaEhValido = false;
+		tdImc.textContent = "Altura inválida";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (pesoEhValido && alturaEhValido) {
+		tdImc.textContent = calculaImc(peso, altura);
+	}
+}
+
+function calculaImc(peso, altura) {
+	var imc = peso / (altura * altura);
+	return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+	if (peso >= 0 && peso <= 1000) {
+	    return true;
+	} else {
+	    return false;
+	}
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3.00) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+
+
