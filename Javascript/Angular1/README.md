03bde9fdf0fee2214c2ad05a6414284428198fb0
Bruno Paulo Costa Lima <bplima@unicid.rede1.br>
2017-09-21 13:35:58 -0300

    add exercises

--- Javascript/Angular1/README.md
+++ Javascript/Angular1/README.md
@@ -1,3 +1,12 @@
 Project Angular 1, curse by AluraOnline
 
-To start server, run: npm start
+Requirements:
+
+Run:
+npm install
+
+If you have any trouble ( npm ERR! self signed certificate) run the command below:
+npm set strict-ssl false
+
+To start server run: 
+npm start
--- Javascript/Angular2/alurapic/client/app/foto/foto.component.js
+++ Javascript/Angular2/alurapic/client/app/foto/foto.component.js
@@ -8,25 +8,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var FotoComponent = /** @class */ (function () {
+var core_1 = require('@angular/core');
+var FotoComponent = (function () {
     function FotoComponent() {
     }
     __decorate([
-        core_1.Input(),
-        __metadata("design:type", Object)
+        core_1.Input(), 
+        __metadata('design:type', Object)
     ], FotoComponent.prototype, "titulo", void 0);
     __decorate([
-        core_1.Input(),
-        __metadata("design:type", Object)
+        core_1.Input(), 
+        __metadata('design:type', Object)
     ], FotoComponent.prototype, "url", void 0);
     FotoComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'foto',
             templateUrl: './foto.component.html'
-        })
+        }), 
+        __metadata('design:paramtypes', [])
     ], FotoComponent);
     return FotoComponent;
 }());
