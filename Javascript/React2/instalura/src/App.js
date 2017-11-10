8ca929811a1613d2b84d60f83d1fb840326e5707
Bruno Paulo Costa Lima <bplima@unicid.rede1.br>
2017-11-10 12:53:48 -0200

    Add exercises of courde React 3 by Alua

--- Javascript/React2/instalura/src/App.js
+++ Javascript/React2/instalura/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {timeline} from './reducers/timeline';
+
+const store = createStore(timeline, applyMiddleware(thunkMiddleware));
 
 class App extends Component {
   render() {
@@ -8,7 +13,7 @@ class App extends Component {
     <div id="root">
       <div className="main">
         <Header/>
-        <Timeline login={this.props.params.login}/>
+        <Timeline login={this.props.params.login} store={store}/>
       </div>
     </div>
     );
