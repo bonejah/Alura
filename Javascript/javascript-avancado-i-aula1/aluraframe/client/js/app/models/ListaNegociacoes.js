002b0e46c35738e7793e984588a75f0f19376db0
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2017-12-11 11:19:40 -0200

    add new exercises Javascript avancado

--- Javascript/javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ Javascript/javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,22 @@
 class ListaNegociacoes {
-    constructor() {
-        this._negociacoes = []
-    }
+  constructor(armadilha) {
+    this._negociacoes = [];
+    this._armadilha = armadilha;
+  }
 
-    adiciona(negociacao) {
-        this._negociacoes.push(negociacao);
-    }
+  adiciona(negociacao) {
+    this._negociacoes.push(negociacao);
+    this._armadilha(this);
+    //Reflect.apply(this._armadilha, this._contexto, [this]);
+  }
 
-    get negociacoes() {
-        return [].concat(this._negociacoes);
-    }
+  get negociacoes() {
+    return [].concat(this._negociacoes);
+  }
 
+  esvazia() {
+    this._negociacoes = [];
+    this._armadilha(this);
+    //Reflect.apply(this._armadilha, this._contexto, [this]);
+  }
 }
