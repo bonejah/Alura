f72b1326e0f62db0e4b1c04b586cb0bb10c156be
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-22 17:52:35 -0200

    new project Grunt

--- Infra/Grunt/README.md
+++ Infra/Grunt/README.md
@@ -1,1 +1,11 @@
 # Course Grunt
+
+## Install
+* npm install grunt --save-dev
+* npm install grunt-cli -g
+* npm install grunt-contrib-copy --save-dev
+* npm install grunt-contrib-clean --save-dev
+
+
+# Links
+* https://gruntjs.com/
---
+++ Infra/Grunt/projeto/Gruntfile.js
@@ -0,0 +1,25 @@
+/* recebe o objeto grunt como parâmetro*/
+module.exports = function(grunt) {
+  grunt.initConfig({
+    /*  suas tasks aqui */
+    copy: {
+      public: {
+        cwd: "public",
+        src: "**",
+        dest: "dist",
+        expand: true
+      }
+    },
+    clean: {
+      dist: {
+        src: "dist"
+      }
+    }
+  });
+
+  grunt.registerTask('dist', ['clean', 'copy']);
+  grunt.registerTask('default', ['dist']);
+  
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks('grunt-contrib-clean');
+};
