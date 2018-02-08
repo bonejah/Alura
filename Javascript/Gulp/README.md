d65744741fd2664510e186522089371b9d92c31b
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-02-08 16:55:05 -0200

    add new exercises

--- Javascript/Gulp/README.md
+++ Javascript/Gulp/README.md
@@ -5,7 +5,13 @@
 * glob pattern: **/* (To copy all content)
 
 ## Install
-* 
+* npm install gulp-clean@0.3.1 --save-dev
+* npm install gulp-concat@2.6.0 --save-dev
+* npm install gulp-html-replace@1.5.4 --save-dev
+* npm install gulp-uglify@1.4.1 --save-dev
+* npm install gulp-usemin@0.3.14 --save-dev
+* npm install gulp-cssmin@0.1.7 --save-dev
+
 
 ## Links
 * https://tinypng.com/
--- Javascript/Gulp/projeto/gulpfile.js
+++ Javascript/Gulp/projeto/gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean');
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,htmlReplace = require('gulp-html-replace')
+  ,uglify = require('gulp-uglify')
+  ,usemin = require('gulp-usemin')
+  ,cssmin = require('gulp-cssmin');
+
+// Task default
+gulp.task('default', ['copy'], function(){
+  //gulp.start('build-img', 'build-html', 'build-js');
+  gulp.start('build-img', 'usemin');
+});
 
 // Task copy
 gulp.task('copy', ['clean'], function() {
@@ -15,8 +26,35 @@ gulp.task('clean', function() {
 });
 
 // Task opmitize images
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
+
+// This task change the tasks commented
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+  .pipe(usemin({
+    js: [uglify],
+    css: [cssmin]
+  }))
+  .pipe(gulp.dest('dist'));
+});
+
+// gulp.task('build-js', function(){
+//   gulp.src(['dist/js/jquery.js', 
+//   'dist/js/home.js', 
+//   'dist/js/produto.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist/js'));
+// });
+
+// gulp.task('build-html', function(){
+//   gulp.src('dist/**/*.html')
+//   .pipe(htmlReplace({
+//     'js':'js/all.js'
+//   }))
+//   .pipe(gulp.dest('dist/'));
+// });
