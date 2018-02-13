6042c32911540ed882ac3344c41425d9069985f7
Bruno Lima <dadivajah@gmail.com>
2018-02-13 21:05:49 -0200

    finish course Gulp

--- Javascript/Gulp/README.md
+++ Javascript/Gulp/README.md
@@ -11,7 +11,12 @@
 * npm install gulp-uglify@1.4.1 --save-dev
 * npm install gulp-usemin@0.3.14 --save-dev
 * npm install gulp-cssmin@0.1.7 --save-dev
-
+* npm install browser-sync@2.9.8 --save-dev
+* npm install gulp-jshint@1.11.2 --save-dev
+* npm install jshint-stylish@2.0.1 --save-dev
+* npm install gulp-csslint@0.2.0 --save-dev
+* npm install gulp-autoprefixer@3.0.2 --save-dev
+* npm install gulp-less@3.0.3 --save-dev
 
 ## Links
 * https://tinypng.com/
--- Javascript/Gulp/projeto/gulpfile.js
+++ Javascript/Gulp/projeto/gulpfile.js
@@ -5,12 +5,55 @@ var gulp = require('gulp')
   ,htmlReplace = require('gulp-html-replace')
   ,uglify = require('gulp-uglify')
   ,usemin = require('gulp-usemin')
-  ,cssmin = require('gulp-cssmin');
+  ,cssmin = require('gulp-cssmin')
+  ,browserSync = require('browser-sync')
+  ,jshint = require('gulp-jshint')
+  ,jshintStylish = require('jshint-stylish')
+  ,csslint = require('gulp-csslint')
+  ,autoprefixer = require('gulp-autoprefixer')
+  ,less = require('gulp-less');
 
 // Task default
 gulp.task('default', ['copy'], function(){
   //gulp.start('build-img', 'build-html', 'build-js');
   gulp.start('build-img', 'usemin');
+});
+
+// Task browsersync
+gulp.task('server', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    }
+  });
+
+  gulp.watch('src/**/*').on('change', browserSync.reload);
+
+  // monitoring less files
+  gulp.watch('src/less/**/*.less').on('change', function(event) {
+    var stream = gulp.src(event.path)
+         .pipe(less().on('error', function(erro) {
+           console.log('LESS, erro compilação: ' + erro.filename);
+           console.log(erro.message);
+         }))
+         .pipe(gulp.dest('src/css'));
+  });
+
+  // monitoring js files
+  gulp.watch('src/js/**/*.js').on('change', function(event) {
+    console.log("Linting " + event.path);
+    gulp.src(event.path)
+        .pipe(jshint())
+        .pipe(jshint.reporter(jshintStylish));
+  });
+
+   // monitoring css files
+   gulp.watch('src/css/**/*.css').on('change', function(event) {
+    console.log("Linting " + event.path);
+    gulp.src(event.path)
+        .pipe(csslint())
+        .pipe(csslint.reporter());
+  });    
 });
 
 // Task copy
@@ -37,7 +80,7 @@ gulp.task('usemin', function() {
   return gulp.src('dist/**/*.html')
   .pipe(usemin({
     js: [uglify],
-    css: [cssmin]
+    css: [autoprefixer, cssmin]
   }))
   .pipe(gulp.dest('dist'));
 });
