bd2f036aed0100d2b2de2344213cf892f9c9f3c8
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-03-01 17:03:59 -0300

    finish course module 1

--- Javascript/Vue1/alurapic/src/components/home/Home.vue
+++ Javascript/Vue1/alurapic/src/components/home/Home.vue
@@ -8,6 +8,10 @@
       <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)"
+            :confirmacao="true" estilo="perigo"/>
+          <meu-botao tipo="button" rotulo="EDITAR" @botaoAtivado="remove(foto)"
+            :confirmacao="false" estilo="padrao"/>
         </meu-painel>
       </li>
     </ul>
@@ -16,12 +20,14 @@
 
 <script>
 import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue';
 
 export default {
   components: {
     'meu-painel' : Painel,
-    'imagem-responsiva' : ImagemResponsiva
+    'imagem-responsiva' : ImagemResponsiva,
+    'meu-botao' : Botao
   },
 
   data() {
@@ -40,6 +46,12 @@ export default {
       } else {
         return this.fotos;
       }
+    }
+  },
+
+  methods: {
+    remove(foto) {
+      alert('Removendo a foto: ' + foto.titulo);
     }
   },
 
