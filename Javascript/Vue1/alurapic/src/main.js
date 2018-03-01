7777f7e0854605a8fc67d2348b3c5f2783b8e789
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-03-01 14:04:23 -0300

    add new exercises Vue

--- Javascript/Vue1/alurapic/src/main.js
+++ Javascript/Vue1/alurapic/src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
+import Vue from 'vue' // Global View
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes,
+  mode: 'history' // Importante para retirar a tralha da URL
+});
+
+// View Instance
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
