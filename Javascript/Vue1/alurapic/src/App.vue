39ca6c318a8572949c5afccf346f78e1abc76e64
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-03-01 14:23:49 -0300

    add exercises

--- Javascript/Vue1/alurapic/src/App.vue
+++ Javascript/Vue1/alurapic/src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="corpo">
-    <nav>
-      <ul>
-        <li v-for="route in routes">
-          <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view></router-view>
+    <meu-menu :routes="routes"></meu-menu>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
  </template>
 
 <script>
 import { routes } from './routes.js';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+  components: {
+    'meu-menu': Menu
+  },
+
   data() {
     return {
       routes
     }
   }
-
 }
 </script>
 
 <style>
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s;
+  }
+
   .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
