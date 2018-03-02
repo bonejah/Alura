23ce6ca3a5a59cb9d1174dac8d44f7c16105f65a
Bruno Lima <dadivajah@gmail.com>
2018-03-02 04:53:30 -0300

    refactors

---
+++ Javascript/Vue/alurapic/src/components/cadastro/Cadastro.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+  </div>
+</template>
+
+<scritp>
+export default {
+
+
+}
+</script>
+
+<style>
+  .centralizado {
+        text-align: center;
+    }
+</style>
