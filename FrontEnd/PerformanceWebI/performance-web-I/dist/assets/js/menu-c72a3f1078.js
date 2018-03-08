4b4215ac62a87e072c6f7c63a057704cd611e30a
Bruno Lima <dadivajah@gmail.com>
2018-03-08 07:15:33 -0300

    add exercises performance-web-I

---
+++ FrontEnd/PerformanceWebI/performance-web-I/dist/assets/js/menu-c72a3f1078.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){var t=document.querySelector(".header-menu"),e=document.querySelector(".header-menu-titulo");e&&(e.onclick=function(){t.hasAttribute("data-ativo")?t.removeAttribute("data-ativo"):t.setAttribute("data-ativo","")})});
