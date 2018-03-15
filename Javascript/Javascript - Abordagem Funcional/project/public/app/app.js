f6270bacf01996481b47a09768fb8a88091bd0e6
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-03-15 14:05:44 -0300

    finish course Javascript - Abordagem Funcional

--- Javascript/Javascript - Abordagem Funcional/project/public/app/app.js
+++ Javascript/Javascript - Abordagem Funcional/project/public/app/app.js
@@ -1,36 +1,23 @@
-import { log }  from './utils/promise-helpers.js'
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js'
 import './utils/array-helpers.js'
 import { notasService as service } from './nota/service.js'
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js'
-
-// document
-//   .querySelector('#myButton')
-//   .onclick = () => 
-//     fetch('http://localhost:3000/notas')
-//       .then(handleStatus)
-//       .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), [])) Functor
-//       .then(notas => notas.$flatMap(nota=> nota.itens))
-//       .then(log)
-//       .then(itens => itens.filter(item => item.codigo == '2143'))
-//       .then(log)
-//       .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-//       .then(console.log)
-//       .catch(console.log)
+import { EventEmitter } from './utils/event-emitter.js'
+import { Maybe } from './utils/maybe.js';
 
 const operations = pipe(
   partialize(debounceTime, 500),
   partialize(takeUntil, 3)
 )
 
-const action = operations(() => 
-    service
-    .sumItems('2143')
-    .then(console.log)
+const action = operations(() =>
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 )
 
 document
   .querySelector('#myButton')
   .onclick = action
-     
+
 
--- Javascript/Javascript - Abordagem Funcional/project/public/app/utils/promise-helpers.js
+++ Javascript/Javascript - Abordagem Funcional/project/public/app/utils/promise-helpers.js
@@ -5,3 +5,27 @@ export const log = param => {
   console.log(param)
   return param
 }
+
+export const timeoutPromise = (miliseconds, promise) => {
+  const timeout = new Promise((resolve, reject) => 
+    setTimeout(() => 
+      reject(`Limite da promise excedido (limite: ${miliseconds} ms)`), miliseconds))
+
+  return Promise.race([
+    timeout, promise
+  ])    
+}
+
+export const delay = miliseconds => data => 
+  new Promise((resolve, reject) => 
+    setTimeout(() => resolve(data), miliseconds)
+)
+
+export const retry = (retries, milliseconds, fn) =>
+  fn().catch(err => {
+    console.log(retries);
+    return delay(milliseconds)().then(() =>
+      retries > 1
+        ? retry(retries - 1, milliseconds, fn)
+        : Promise.reject(err))
+  });
